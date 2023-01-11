0e1ffe38976a6e15548c8aa6b64aa80e5054bc07
Больбот <bolbotandrii@gmail.com>
2023-01-11 17:23:46 +0200

    init

---
+++ client/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Head from "next/head"
+
+const Index = () => {
+    return (
+        <>
+            <Head>
+                <title>Main page</title>
+            </Head>
+            <h1>
+                Articles
+            </h1>
+        </>
+    )
+}
+
+export default Index
